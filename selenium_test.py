10d65a66324475a21ebb149f983a59663e05ea8c
Gui Ruggiero <guilherme.ruggiero@gmail.com>
2024-05-18 00:32:21 +0200

    Selenium working on Chromebook

--- selenium_test.py
+++ selenium_test.py
@@ -1,8 +1,12 @@
 from selenium import webdriver
 import time
 
+# install selenium https://pypi.org/project/selenium/
+# install chrome driver https://chromedriver.chromium.org/downloads
+
 browser = webdriver.Chrome()
 
 browser.get("http://www.google.com")
 time.sleep(5)
+
 browser.close()

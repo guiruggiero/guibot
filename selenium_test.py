47f8dc034f8310407e065228ce5f3cfe0d98bea4
Gui Ruggiero <guilherme.ruggiero@gmail.com>
2024-05-18 01:52:24 +0000

    Server test - not finding Chrome binary

--- selenium_test.py
+++ selenium_test.py
@@ -1,12 +1,13 @@
-from selenium import webdriver
-import time
+# Install Selenium https://pypi.org/project/selenium/
+# Install Chrome driver https://chromedriver.chromium.org/downloads
 
-# Install selenium https://pypi.org/project/selenium/
-# Install chrome driver https://chromedriver.chromium.org/downloads
+from time import sleep
+from selenium import webdriver
 
 browser = webdriver.Chrome()
 
 browser.get("http://www.google.com")
-time.sleep(5)
+print("Page opened\n")
+sleep(3)
 
 browser.close()
